a54fa53c6a81b23e627f09c91bdfc7aaa4a56fcd
Chia Hao Hsu Tai <hsutai.ch@northeastern.edu>
2026-05-17 14:55:29 -0400

    new svelte project files

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Notepad++</title>
+</svelte:head>
+
+<div class="h-full w-full space-y-4 px-12 py-8">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="space-y-2">
+	<h1 class="text-4xl">Notepad++</h1>
+	<p>
+		Collaborate with friends in real time, share notes instantly, and work together seamlessly with
+		live updates and synchronized editing.
+	</p>
+</div>
+<h2 class="text-2xl">Your Notes</h2>
+<div></div>
